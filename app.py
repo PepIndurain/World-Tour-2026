9df99c11f9e10a1afbbd2b20185972620a347436
PepIndurain <giuslazzaroni@alice.it>
2026-05-01 11:44:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide", page_title="Cycling Pro Hub")
 # Quando avrai l'URL della Volta, sostituisci 'YOUR_VOLTA_URL_HERE'
 TOURS = {
     "Itzulia Basque Country": "https://script.google.com/macros/s/AKfycbzQ-ORFurfO95nLnljLP4Z5eMJQv5bzE8k5voX_CrKhpNTemYaeoD8UNftr2p1ClJWr/exec",
-    "Volta Ciclista a Catalunya": "YOUR_VOLTA_URL_HERE",
+    "Volta Ciclista a Catalunya": "https://script.google.com/macros/s/AKfycbxXHl_6r4aSzKUo7ziahiDp08DiSKRCobOt3Ecu29n71-PnwI1ipRrbgH7GeeHw7NKV/exec",
 }
 
 # --- 2. GITHUB IMAGES CONFIGURATION ---
